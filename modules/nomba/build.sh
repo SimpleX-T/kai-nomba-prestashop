171159a81873fefdcd141a515aec7ceb4b870e52
SimpleX-T <ntmark2004@gmail.com>
2026-06-30 09:30:00 +0100

    fix: build script path resolution and version extraction

--- modules/nomba/build.sh
+++ modules/nomba/build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
 
-MODULE="nomba"
-VERSION=$(grep -oP "'version'\s*=>\s*'\K[^']+" "$MODULE/nomba.php")
-OUTPUT="nomba-prestashop-${VERSION}.zip"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+MODULE_DIR="$(basename "$SCRIPT_DIR")"
+PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+
+cd "$PARENT_DIR"
+
+VERSION=$(grep -oP '\$this->version\s*=\s*'\''\K[^'\'']+' "$MODULE_DIR/$MODULE_DIR.php")
+OUTPUT="$MODULE_DIR/nomba-prestashop-${VERSION}.zip"
 
 rm -f "$OUTPUT"
 
-zip -r "$OUTPUT" "$MODULE" \
-    -x "$MODULE/.git/*" \
-    -x "$MODULE/.env" \
+zip -r "$OUTPUT" "$MODULE_DIR" \
+    -x "$MODULE_DIR/.git/*" \
+    -x "$MODULE_DIR/.env" \
     -x "*/.*" \
     -x "*/vendor/*"
 
